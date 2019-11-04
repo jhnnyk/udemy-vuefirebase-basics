d8009c123eb7b112d064779b4748afe1d82bf090
John Knotts <jhnnyk@gmail.com>
2019-11-04 15:07:01 -0700

    data binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#app',
   data: {
     title: 'Becoming a Vue ninja',
-    name: 'John'
+    name: 'John',
+    url: 'http://www.youtube.com',
+    classes: ['one', 'two']
   },
   methods: {
     greet(time) {
